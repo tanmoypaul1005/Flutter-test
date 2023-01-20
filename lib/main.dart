e412cef312d9ff28dc68f1fc0f97ca2ad45363d2
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2023-01-20 15:19:03 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const GeeksForGeeks());
+}
+
+class GeeksForGeeks extends StatelessWidget {
+  const GeeksForGeeks({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeActivity(),
+    );
+  }
+}
+
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Hello World----------')),
+    );
+  }
+}
