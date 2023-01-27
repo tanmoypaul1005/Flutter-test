00b67c242bb2b3e3669ebc963cdba42d2f70dbd2
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2023-01-27 11:48:27 +0600

    OK

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,10 @@ class HomeActivity extends StatelessWidget {
           toolbarHeight: 60,
         ),
         body: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(height: 100,width: 100,child: Image.network("https://scontent.fdac31-1.fna.fbcdn.net/v/t39.30808-6/300960294_101330042720115_301174734956893395_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeGU-y7KIBKJAlemJQn-emzX1cudNoqjMMLVy502iqMwwtGT91X64AxXUwzBwnSMsmR2ns_4dtfEL7AIGn9K8w-s&_nc_ohc=7fRnziHfKt8AX8EShBW&_nc_ht=scontent.fdac31-1.fna&oh=00_AfBK5ugZrBBo8VSFMD8Dlv-dQXEMbGj56VB_ePKs86SEZg&oe=63D9045C"),)
+          ],
         ));
   }
 }
