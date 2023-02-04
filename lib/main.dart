f07135850e5bf6eeb53517856e3699ea83cb9442
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2023-02-04 14:34:45 +0600

    card

--- lib/main.dart
+++ lib/main.dart
@@ -26,67 +26,21 @@ class HomeActivity extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('HomeActivity'),
-        centerTitle: true,
-        toolbarHeight: 60,
-      ),
-      body: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-        ElevatedButton(
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => Activity1("This is Activity 1")),
-);
-            },
-            child: Text("Button1")),
-        ElevatedButton(
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => Activity2("This is Activity 2")));
-            },
-            child: Text("Button2")),
-      ]),
-    );
-  }
-}
-
-class Activity1 extends StatelessWidget {
-  String msg;
-   Activity1(this.msg,{Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(msg),
-        centerTitle: true,
-        toolbarHeight: 60,
-      ),
-      body: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-        ElevatedButton(
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => Activity2("This is Activity 2")));
-            },
-            child: Text("Button2")),
-      ]),
-    );
-  }
-}
-
-
-class Activity2 extends StatelessWidget {
-  String msg;
-   Activity2(this.msg,{Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(msg),
-        centerTitle: true,
-        toolbarHeight: 60,
-      ),
-    );
+        appBar: AppBar(
+          title: Text('HomeActivity'),
+          centerTitle: true,
+          toolbarHeight: 60,
+        ),
+        body: Center(
+          child: Card(
+            elevation: 80,
+            child: SizedBox(
+                height: 200,
+                width: 200,
+                child: Center(
+                  child: Text("ok"),
+                )),
+          ),
+        ));
   }
 }
